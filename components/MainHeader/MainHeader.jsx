c081a00f684cad623c46d50779b248649fe5495e
Rauf Erk <erk.rauf@gmail.com>
2025-01-06 19:57:20 +0300

    part 102 is ready

--- components/MainHeader/MainHeader.jsx
+++ components/MainHeader/MainHeader.jsx
@@ -2,32 +2,36 @@ import LogoPic from '@/assets/logo.png'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './styles.module.css'
+import HeaderBackground from '@/components/HeaderBackground'
 
 const MainHeader = () => {
   const { logo, header, nav } = styles
   return (
-    <header className={header}>
-      <Link className={logo} href='/'>
-        <Image
-          src={LogoPic.src}
-          alt='A plate with food on it'
-          width={1024}
-          height={1024}
-          priority
-        />
-        NextLevel Food
-      </Link>
-      <nav className={nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <HeaderBackground />
+      <header className={header}>
+        <Link className={logo} href='/'>
+          <Image
+            src={LogoPic.src}
+            alt='A plate with food on it'
+            width={1024}
+            height={1024}
+            priority
+          />
+          NextLevel Food
+        </Link>
+        <nav className={nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
